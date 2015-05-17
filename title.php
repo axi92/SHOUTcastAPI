ec0fe6bd2bd03052276d61b0641f27cdff1830ae
axi92 <phips_92@yahoo.de>
2015-05-17 22:49:27 +0200

    title.php finished
    Workes finde on localhost but online no...

--- title.php
+++ title.php
@@ -5,16 +5,16 @@ function get_web_page( $url )
     $options = array(
         CURLOPT_RETURNTRANSFER => true,     // return web page
         CURLOPT_HEADER         => false,    // don't return headers
-        CURLOPT_NOBODY         => true,    // don't return headers
         CURLOPT_FOLLOWLOCATION => true,     // follow redirects
         CURLOPT_ENCODING       => "",       // handle all encodings
-        CURLOPT_USERAGENT      => "Title graber", // who am i
+        CURLOPT_USERAGENT      => "Mozilla/5.0 (Windows NT 6.3; WOW64; rv:34.0) Gecko/20100101 Firefox/34.0", // who am i
         CURLOPT_AUTOREFERER    => true,     // set referer on redirect
         CURLOPT_CONNECTTIMEOUT => 5,      // timeout on connect
         CURLOPT_TIMEOUT        => 5,      // timeout on response
         CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
         CURLOPT_PORT           => 1337,     // port
-        CURLOPT_VERBOSE        => true,     // debuging option
+        CURLOPT_VERBOSE        => false,     // debuging option
+        CURLOPT_SSL_VERIFYPEER => false,     // debuging option
     );
 
     $ch      = curl_init( $url );
@@ -28,24 +28,22 @@ function get_web_page( $url )
     $header['errno']   = $err;
     $header['errmsg']  = $errmsg;
     $header['content'] = $content;
-    //return $header['content'];
-    return $header;
+    return $header['content'];
 }
-echo '<pre>';
-echo print_r(get_web_page($url));
-//$file = trim(get_web_page($url));
+$file = trim(get_web_page($url));
 
-/*
-$pattern1 = '~Players Online: </font><font size="2" color="#BBBBBB"><b>[0-9]*</b></font></td>~';
+
+$pattern1 = '~Current Song: <\/font><\/td><td><font class=default><b>[a-zA-Z0-9 .-]*~';
 preg_match_all($pattern1, $file, $matches);
 $string = json_encode($matches[0]);
-//["Players Online: <\/font>61612<\/b><\/font><\/td>"]
-$string = str_replace('<\/b><\/font><\/td>"]', '', $string);
-$string = str_replace('["Players Online: <\/font>', '', $string);
+$string = str_replace('Current Song: <\/font><\/td><td><font class=default><b>', '', $string);
+$string = str_replace('["', '', $string);
+$string = str_replace('"]', '', $string);
+/*
 $string = str_replace('<font size=\"2\" color=\"#BBBBBB\"><b>', '', $string);
-echo '<pre>';
-echo $string;
-echo '</pre>';
 */
+//echo '<pre>';
+echo $string;
+//echo '</pre>';
 
 ?>
